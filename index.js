1a7afcd0c981496563def3b1065b8a542a7c4e39
Ethan Murphy <verakai141@gmail.com>
2019-01-14 20:36:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+//BEATLES LOOPS:
+
+//Combiner for loop
+function theBeatlesPlay(primary, secondary){
+var result=[];
+for (let i=0; i<4; i++){
+result.push(`${primary[i]} plays ${secondary[i]}`);
+  }
+return result
+}
+
+//Exclamator while loop
+function johnLennonFacts(primary){
+var result=[];
+let i=0;
+while(i < primary.length ){
+result.push(`${facts[i]}!!!`) 
+i++
+	}
+return result;
+}
+
+
+
+//"I love the beatles" do-while loop
+
